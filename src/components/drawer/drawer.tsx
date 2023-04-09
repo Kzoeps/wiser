2ea3f7b02119347ece6474eb824bd6bfca2f4a84
kzoeps <kzoepa@gmail.com>
2023-04-09 16:48:02 -0400

    feat: add logic to display active chat

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -1,74 +1,96 @@
 import {
-  Input, Box,
+  Input,
+  Box,
   Flex,
   Heading,
   IconButton,
   InputGroup,
-  InputLeftElement
+  InputLeftElement,
 } from "@chakra-ui/react";
 import { HiPencilSquare, HiMagnifyingGlass } from "react-icons/hi2";
 import ChatsDisplay from "../chats-display/chats-display";
 import { IConversation } from "../../types/wiser.types";
+import { NavLink } from "react-router-dom";
+import styles from "./drawer.module.css";
 
 export interface WiserDrawerProps {
   conversations: IConversation[];
-  onCompose?: () => void
+  onCompose?: () => void;
   onSearch: (search: string) => void;
 }
 
-export default function WiserDrawer({ conversations, onSearch, onCompose }: WiserDrawerProps) {
-
+export default function WiserDrawer({
+  conversations,
+  onSearch,
+  onCompose,
+}: WiserDrawerProps) {
   const spliceMessage = (message: string | undefined) => {
-    if (!message) return '';
+    if (!message) return "";
     if (message.length > 30) {
       return `${message.slice(0, 30)}...`;
     }
-    return message
-  }
+    return message;
+  };
   return (
     <>
       <Box
         borderRight={"1px solid"}
         borderRightColor={"gray.100"}
         maxW={"300px"}
-        overflowY={'auto'}
+        overflowY={"auto"}
         height={"100vh"}
       >
-        <Box mb={1} borderBottom={'1px solid'} borderBottomColor={'gray.100'}>
-        <Flex direction={"row"} justifyContent={"space-between"}>
-          <Box maxW={"150px"}>
-            <Heading m={2}>Chats</Heading>
+        <Box mb={1} borderBottom={"1px solid"} borderBottomColor={"gray.100"}>
+          <Flex direction={"row"} justifyContent={"space-between"}>
+            <Box maxW={"150px"}>
+              <Heading m={2}>Chats</Heading>
+            </Box>
+            <Box maxW={"150px"}>
+              <IconButton
+                onClick={onCompose || (() => {})}
+                m={2}
+                variant={"outline"}
+                size={"sm"}
+                borderRadius={"50%"}
+                aria-label="compose"
+                icon={<HiPencilSquare />}
+              />
+            </Box>
+          </Flex>
+          <Box maxW={"300px"} p={2}>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<HiMagnifyingGlass />}
+              />
+              <Input
+                onChange={(e) => {
+                  onSearch(e.target.value);
+                }}
+                borderRadius={"lg"}
+                type="tel"
+                placeholder="Search chat here"
+              />
+            </InputGroup>
           </Box>
-          <Box maxW={"150px"}>
-            <IconButton
-              onClick={onCompose || (() => {})}
-              m={2}
-              variant={"outline"}
-              size={"sm"}
-              borderRadius={"50%"}
-              aria-label="compose"
-              icon={<HiPencilSquare />}
-            />
-          </Box>
-        </Flex>
-        <Box maxW={"300px"} p={2}>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<HiMagnifyingGlass />}
-            />
-            <Input
-              onChange={(e) => { onSearch(e.target.value) }}
-              borderRadius={"lg"}
-              type="tel"
-              placeholder="Search chat here"
-            />
-          </InputGroup>
-        </Box>
         </Box>
         <Box>
           {conversations.map((conversation) => (
-            <ChatsDisplay key={conversation.id} title={conversation.title} message={spliceMessage(conversation.messages.at(-1)?.content)}/>
+            <NavLink
+              to={`${conversation.id}`}
+              className={({ isActive }) =>
+                isActive ? `${styles.active}` : ""
+              }
+            >
+              {({isActive}) => {
+                return <ChatsDisplay
+                isActive={isActive}
+                key={conversation.id}
+                title={conversation.title}
+                message={spliceMessage(conversation.messages.at(-1)?.content)}
+              />
+              }}
+            </NavLink>
           ))}
         </Box>
       </Box>
